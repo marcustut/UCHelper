af604aaac30a200f2dd04fe432c71247515d43dd
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2021-03-16 16:32:43 +0800

    Added files from cecelia

---
+++ src/main/java/UCHelper/adt/FlexibleQueueInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt;
+
+/**
+ *
+ * @author Cecelia Lim Jie Shi RSFY2S2
+ */
+public interface FlexibleQueueInterface <E>{
+    int size();
+    boolean isEmpty();
+    E first();
+    E last();
+    void rotate();
+    void addFirst(E e);
+    void addLast(E e);
+    E removeFirst();
+    void display();
+    void clear();
+   
+}
